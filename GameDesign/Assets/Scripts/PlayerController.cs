02452a66006358a141b0d572699ba7a4d2e4d62d
AngelPalomares <64100727+AngelPalomares@users.noreply.github.com>
2022-04-01 19:44:29 -0400

    Update

---
+++ GameDesign/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+	//Rigidbody of the player
+	[SerializeField]
+	private Rigidbody2D TheRB;
+	//The speed of the play
+	[SerializeField]
+	private float movespeed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//function that makes the player move
+		TheRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * movespeed;
+	}
+}
